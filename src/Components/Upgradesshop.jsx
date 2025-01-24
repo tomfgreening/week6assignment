6e8acf585d11023b28782a211e99f25054120e56
Thomas Greening <tomfgreening@gmail.com>
2025-01-24 18:00:36 +0000

    Left some notes to pickup from tommorow.

--- src/Components/Upgradesshop.jsx
+++ src/Components/Upgradesshop.jsx
@@ -1,3 +1,12 @@
 export default function Upgradesshop(props) {
   return <p>{props.upgradesShopText}</p>;
+
+//   export default function App() {
+//     const items = ["Item 1", "Item 2", "Item 3"];
+//     const htmlItems = items.map((item) => <li>{item}</li>);
+  
+//     return <ul>{htmlItems}</ul>;
+
+// above code is from LISTS and KEYS workshop. Pickup from here tommorow morning.
+  }
 }
