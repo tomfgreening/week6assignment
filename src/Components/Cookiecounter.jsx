3a58139ceb5f84615ea96deaea26c110208f6a37
Thomas Greening <tomfgreening@gmail.com>
2025-01-25 20:41:04 +0000

    cookie counter troubleshooting.

--- src/Components/Cookiecounter.jsx
+++ src/Components/Cookiecounter.jsx
@@ -3,18 +3,66 @@ import Clikr from "./Clickerbutton";
 import Timerclean from "./Timerclean";
 import Cookietimer from "./Cookietimer";
 
-
 export default function Cookiecounter() {
-  //this block of code is very useful. What does it do?
+  const [cookieCount, setCookieCount] = useState(0);
+  const [cps, setCPS] = useState(1);
+
   useEffect(() => {
-    const myInterval = setInterval(() => {
-      setCookies((currentCookies) => {
-        currentCookies + cps;
-      });
+    const interval = setInterval(() => {
+      setCookieCount((cookieCount) => cookieCount + cps);
     }, 1000);
+
     return () => {
-      clearInterval(myInterval);
+      clearInterval(interval);
     };
   }, [cps]);
 
-//   I know the code above adds the clicked cookies and cookies per second total but i'm pulling my hair out trying to get it to function as intended.
+  return (
+    <div>
+      <h1>{cookieCount}</h1>
+      {cps}
+    </div>
+  );
+}
+
+/* // useEffect(() => {
+//   const myInterval = setInterval(() => {
+//     setCookies((currentCookies) => {
+//       currentCookies + cps;
+//     });
+//   }, 1000);
+//   return () => {
+//     clearInterval(myInterval);
+//   };
+// }, [cps]); */
+
+// import { useState, useEffect } from "react";
+
+// export default function CookieCounter() {
+//   const [cookieCount, setCookieCount] = useState(0);
+//   const [cps, setCPS] = useState(1);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCookieCount((cookieCount) => cookieCount + cps);
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+//   }, [cps]);
+
+//   return (
+//     <div>
+//       <h1>🍪 Cookies: {cookieCount}</h1>
+//       <h2>⚡ Cookies Per Second (CPS): {cps}</h2>
+
+//       {/* Clicker button */}
+//       <button onClick={() => setCookieCount(cookieCount + 1)}>
+//         🍪 Click me for a cookie!
+//       </button>
+
+//       {/* Upgrade button */}
+//       <button onClick={() => setCPS(cps + 1)}>
+//         ⚡ Buy Upgrade (+1 CPS)
+//       </button>
+//     </div>
+//   );
