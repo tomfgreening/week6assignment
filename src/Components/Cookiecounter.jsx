2c1372212b2e0d130307d8b4663691019c79c3e2
Thomas Greening <tomfgreening@gmail.com>
2025-01-26 14:39:54 +0000

    further troubleshooting.

--- src/Components/Cookiecounter.jsx
+++ src/Components/Cookiecounter.jsx
@@ -19,50 +19,8 @@ export default function Cookiecounter() {
 
   return (
     <div>
-      <h1>{cookieCount}</h1>
+      <h1>{cookieCount + cps}</h1>
       {cps}
     </div>
   );
 }
-
-/* // useEffect(() => {
-//   const myInterval = setInterval(() => {
-//     setCookies((currentCookies) => {
-//       currentCookies + cps;
-//     });
-//   }, 1000);
-//   return () => {
-//     clearInterval(myInterval);
-//   };
-// }, [cps]); */
-
-// import { useState, useEffect } from "react";
-
-// export default function CookieCounter() {
-//   const [cookieCount, setCookieCount] = useState(0);
-//   const [cps, setCPS] = useState(1);
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setCookieCount((cookieCount) => cookieCount + cps);
-//     }, 1000);
-
-//     return () => clearInterval(interval);
-//   }, [cps]);
-
-//   return (
-//     <div>
-//       <h1>🍪 Cookies: {cookieCount}</h1>
-//       <h2>⚡ Cookies Per Second (CPS): {cps}</h2>
-
-//       {/* Clicker button */}
-//       <button onClick={() => setCookieCount(cookieCount + 1)}>
-//         🍪 Click me for a cookie!
-//       </button>
-
-//       {/* Upgrade button */}
-//       <button onClick={() => setCPS(cps + 1)}>
-//         ⚡ Buy Upgrade (+1 CPS)
-//       </button>
-//     </div>
-//   );
